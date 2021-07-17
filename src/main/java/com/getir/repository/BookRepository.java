e317095c05890bfdf441220360643d5430b24897
ccanozerr <ccanozerr@gmail.com>
2021-07-17 12:51:04 +0300

    revert commit 476efdc

--- src/main/java/com/getir/repository/BookRepository.java
+++ src/main/java/com/getir/repository/BookRepository.java
@@ -2,17 +2,6 @@ package com.getir.repository;
 
 import com.getir.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-
-import javax.persistence.LockModeType;
-import java.util.List;
-import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<Book> findById(Long id);
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    List<Book> findAllById(List<Long> ids);
 }
